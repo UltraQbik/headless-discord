b62f7c2c6da0548022618172cdbc6e79b4c39b31
UltraQbik <no1skill@yandex.ru>
2024-04-05 00:10:17 +0300

    poorly working thing
    poorly, because the heartbeat is being processed incorrectly

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 import asyncio
 import argparse
 import websockets
+from typing import Any
+from random import random
 
 
 parser = argparse.ArgumentParser(
@@ -16,7 +18,9 @@ class Client:
     def __init__(self):
         self._auth_token: str | None = None
         self._socket: websockets.WebSocketClientProtocol | None = None
+
         self._heartbeat_interval = None
+        self._sequence = None
 
     def run(self, token: str) -> None:
         """
@@ -26,20 +30,77 @@ class Client:
         self._auth_token = token
 
         async def coro():
-            async with websockets.connect("wss://gateway.discord.gg/?v=10&encoding=json") as websocket:
+            async with websockets.connect("wss://gateway.discord.gg/?v=9&encoding=json") as websocket:
                 self._socket = websocket
+                self._heartbeat_interval = (await self.get_request())["d"]["heartbeat_interval"]
 
-                print(await self.get_request())
+                await self.send_request(
+                    {
+                        "op": 2,
+                        "d": {
+                            "token": self._auth_token,
+                            "capabilities": 16381,
+                            "properties": {
+                                "os": "Windows",
+                                "browser": "Chrome",
+                                "device": "",
+                                "system_locale": "en-US",
+                                "browser_user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                                                      "(KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+                                "browser_version": "123.0.0.0",
+                                "os_version": "10",
+                                "referrer": "https://search.brave.com/",
+                                "referring_domain": "search.brave.com",
+                                "referrer_current": "",
+                                "referring_domain_current": "",
+                                "release_channel": "stable",
+                                "client_build_number": 281369,
+                                "client_event_source": None
+                            }
+                        }
+                    }
+                )
+
+                await asyncio.gather(
+                    self.process_heartbeat(),
+                    self.process_input())
+
         asyncio.run(coro())
 
-    async def send_request(self, request: object) -> None:
+    async def process_input(self) -> None:
+        """
+        Processes gateways input things
+        """
+
+        while True:
+            response = await self.get_request()
+            self._sequence = response["s"]
+            print(response)
+
+    async def process_heartbeat(self) -> None:
+        """
+        Sends heartbeat event to opened gateway, to notify it that the app is running.
+        Ran only once, when the connection is opened
+        """
+
+        # send heartbeat
+        # wait `heartbeat_interval * jitter` as per discord docs
+        await asyncio.sleep(self._heartbeat_interval * random() / 1000)
+        await self.send_request({"op": 1, "d": None})
+
+        # continue the heartbeat
+        while self._socket.open:
+            await asyncio.sleep(self._heartbeat_interval)
+            await self.send_request({"op": 1, "d": self._sequence})
+
+    async def send_request(self, request: Any) -> None:
         """
         Sends a request to connected socket
         """
 
-        pass
+        await self._socket.send(json.dumps(request))
 
-    async def get_request(self) -> object:
+    async def get_request(self) -> Any:
         """
         Gets request from connected socket
         """

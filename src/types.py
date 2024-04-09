6ec806adfb841314a6ef82084ed4d46b17358b3c
UltraQbik <no1skill@yandex.ru>
2024-04-10 01:09:19 +0300

    ChannelType enum

--- src/types.py
+++ src/types.py
@@ -1,5 +1,5 @@
 import re
-from enum import Flag, auto
+from enum import Enum, Flag, auto
 
 from .constants import *
 from datetime import datetime
@@ -59,6 +59,26 @@ class Permissions(Flag):
     SEND_VOICE_MESSAGES = auto()
 
 
+class ChannelType(Enum):
+    """
+    Channel type enum
+    """
+
+    GUILD_TEXT = 0
+    DM = 1
+    GUILD_VOICE = 2
+    GROUP_DM = 3
+    GUILD_CATEGORY = 4
+    GUILD_ANNOUNCEMENT = 5
+    ANNOUNCEMENT_THREAD = 10
+    PUBLIC_THREAD = 11
+    PRIVATE_THREAD = 12
+    GUILD_STAGE_VOICE = 13
+    GUILD_DIRECTORY = 14
+    GUILD_FORUM = 15
+    GUILD_MEDIA = 16
+
+
 class Role:
     """
     Role class
@@ -105,7 +125,7 @@ class Channel:
     def __init__(self, **kwargs):
         self.id: str = kwargs.get("id")
         self.guild: Guild | None = Client.known_guilds.get(kwargs.get("guild_id"))
-        self.type: int = kwargs.get("type")
+        self.type: ChannelType = ChannelType(kwargs.get("type"))
         self.name: str | None = kwargs.get("name")
         self.position: int = kwargs.get("position", 0)
         self.permissions: Permissions | None = Permissions(
